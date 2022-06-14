21aca9df563d4744f0556b25f732a0a7e59555cc
MendiDev <107432200+MendiDev@users.noreply.github.com>
2022-06-14 15:04:07 +0300

    fix error

--- app/src/main/java/com/medisafe/sdkexample/MainActivity.kt
+++ app/src/main/java/com/medisafe/sdkexample/MainActivity.kt
@@ -2,12 +2,11 @@ package com.medisafe.sdkexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.medisafe.mylibrarya.LibA
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        LibA().run()
+        //LibA().run()
     }
 }
